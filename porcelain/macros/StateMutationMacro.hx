354ed02c4ad54bcab7abec720f1c5a24e0436dcf
inc0der <inc0der.dev@gmail.com>
2023-08-22 21:15:32 -0300

    add state Mutation interface and macro

---
+++ porcelain/macros/StateMutationMacro.hx
@@ -0,0 +1,50 @@
+package;
+
+import haxe.macro.Type;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+import haxe.macro.TypeTools;
+
+using Lambda;
+using StringTools;
+
+class StateMutationMacro {
+  #if macro
+  public static function build(): Array<Field> {
+    var localFields = Context.getBuildFields();
+
+    for (lf in localFields) {
+      switch lf.kind {
+        case FFun(f):
+          if (lf.access.contains(AStatic)) {
+            var name = lf.name;
+            var args: Array<FunctionArg> = f.args;
+            var argsForMeta = [];
+
+            for (arg in args) {
+              switch arg.type {
+                case TPath(s):
+                  argsForMeta.push({
+                    name: arg.name,
+                    opt: arg.opt,
+                    typeName: s.name
+                  });
+                case _:
+              }
+            }
+
+            // var type
+            lf.meta.push({
+              name: 'tempFieldData',
+              params: [macro $v{name}, macro $v{argsForMeta}],
+              pos: lf.pos
+            });
+          }
+        case _:
+      }
+    }
+
+    return localFields;
+  }
+  #end
+}
---
+++ porcelain/store/Mutation.hx
@@ -0,0 +1,4 @@
+package porcelain.store;
+
+@:autoBuild(porcelain.macros.StateMutationMacro.build())
+interface Mutation {}
